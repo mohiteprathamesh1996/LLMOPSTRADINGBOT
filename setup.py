3439afb1a7506baf063850a8094fa7f569d8bbff
mohiteprathamesh1996 <mohite.p@northeastern.edu>
2024-06-22 10:57:46 -0400

    First commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup 
+
+
+setup(
+    name="LLMOps Trading Bot",
+    version="0.0.0",
+    author="Prathamesh Mohite",
+    author_email="prathamesh.mohite96@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+    )  
+
+
+
+
+
