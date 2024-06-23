619a8bda6b04312ffdc6f31a7423fae3d02c4c9f
Mohite <PMOHITE@BWH.HARVARD.EDU>
2024-06-23 10:20:59 -0400

    Updated code & requirements

--- setup.py
+++ setup.py
@@ -1,16 +1,10 @@
 from setuptools import find_packages, setup 
 
-
 setup(
-    name="LLMOps Trading Bot",
-    version="0.0.0",
+    name="financebot",
+    version="0.0.1",
     author="Prathamesh Mohite",
     author_email="prathamesh.mohite96@gmail.com",
     packages=find_packages(),
     install_requires=[]
     )  
-
-
-
-
-
